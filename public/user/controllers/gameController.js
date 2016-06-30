34f44ae957a143cada15e7de74f1afd9449f1b1e
mustafa225 <mustafalim225@gmail.com>
2016-06-30 15:27:51 +0700

    fix comment game problem

--- public/user/controllers/gameController.js
+++ public/user/controllers/gameController.js
@@ -2,6 +2,11 @@ angular.module('gameModule', [])
     .controller('gameController', ['$scope','$routeParams','$location','game','gameComment', 'amazonData',
         function($scope, $routeParams, $location, game, gameComment, amazonData) {
 
+        $scope.comment_name = "Name";
+        $scope.comment_email = "Email";
+        $scope.comment_subject = "Subject";
+        $scope.comment_comment = "Message";
+
         $scope.game_id = $routeParams.id;
         game.get($scope.game_id)
             .success(function(data){
@@ -23,4 +28,18 @@ angular.module('gameModule', [])
                 $scope.gameLink = data.link;
                 $scope.lowestPrice = data.lowestPrice;
             });
+
+        $scope.postComment = function() {
+            gameComment.post($scope)
+                .success(function(data){
+                    gameComment.cget($scope.game_id)
+                        .success(function(data){
+                            $scope.gameComments = data;
+                            $scope.comment_name = "Name";
+                            $scope.comment_email = "Email";
+                            $scope.comment_subject = "Subject";
+                            $scope.comment_comment = "Message";
+                        });
+                });
+        }
     }]);
--- public/user/services/gameCommentService.js
+++ public/user/services/gameCommentService.js
@@ -1,8 +1,18 @@
 angular.module('gameCommentService', [])
-    .factory('gameComment', ['$http',function($http) {
+    .factory('gameComment', ['$http',
+        function($http) {
             return {
                 cget: function(game_id) {
                     return $http.get('/api/game/comment/' + game_id);
                 },
+                post: function($scope) {
+                    return $http.post('/api/game/comment/add', {
+                        'game_id': $scope.game_id,
+                        'name': $scope.comment_name,
+                        'email': $scope.comment_email,
+                        'subject': $scope.comment_subject,
+                        'comment': $scope.comment_comment
+                    });
+                }
             }
-    }]);
+        }]);
